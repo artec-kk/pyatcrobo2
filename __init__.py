e34e0219c499814d2c43f806618995e0012f771b
skagayama <s-kagayama@artec-kk.co.jp>
2020-06-22 21:16:22 +0900

    下記の修正 - 超音波センサーのimport - カラーセンサーの値を参照からコピーを返すように変更

--- __init__.py
+++ __init__.py
@@ -8,5 +8,7 @@ from .parts import LightSensor
 from .parts import SoundSensor
 from .parts import TouchSensor
 from .parts import Temperature
+from .parts import UltrasonicSensor
 from .parts import Accelerometer
 # from parts import Gyro
+
